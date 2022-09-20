096c97982a41a13cd6c97b75a36cb5dff6ecac83
Rauno Viskus <rauno56@gmail.com>
2022-09-20 16:26:56 +0300

    chore: filter empty lines out from lerna output

--- scripts/parse-lerna-scopes.mjs
+++ scripts/parse-lerna-scopes.mjs
@@ -1,9 +1,12 @@
 import * as childProcess from 'child_process';
 
 const labels = JSON.parse(process.argv[2]);
-const packageList = new Set(childProcess.spawnSync('lerna', ['list']).stdout
-	.toString('utf8')
-	.split('\n'));
+const packageList = new Set(
+	childProcess.spawnSync('lerna', ['list']).stdout
+		.toString('utf8')
+		.split('\n')
+		.filter(Boolean)
+);
 
 console.error('Labels:', labels);
 console.error('Packages:', [...packageList]);
